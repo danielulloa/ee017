86387508940e93807c87c793084fbf83f8562180
vyrodriguez <anavictoriarodriguez94@gmail.com>
2020-01-29 00:40:43 -0300

    Agrega ejercicios 2 y 3

---
+++ trabajo_final/sketchs_matlab/Ejercicios pasados/Ejercicion3.m
@@ -0,0 +1,107 @@
+%Ejercicio nº3
+clc;
+clear;
+
+%Valores de los componentes
+R1=1;
+R2=1;
+L1=1;
+C1=1;
+C2=1;
+C3=1;
+E=[];
+
+%Condiciones Iniciales
+vc1=1;
+vc2=0;
+
+w=1;
+
+%Valores de tiempo y paso
+ti=0;
+tf=6*pi;
+h=0.1;
+
+%Matrices del circuito 
+%Lleva la forma de:
+%M*(dx/dt)+N*x=u(t);
+
+M=[C3 -C2;C1+C3 C1];
+N=[0 -1/R2; 1/R1 1/R1];
+u=[0;E/R1];
+
+%Condiciones iniciales
+Xant=[vc1;vc2];
+
+%Se lleva a la forma 
+% dx/dt=q(t)-P*x
+
+P=-1.*(M\N);
+
+solu=[];
+
+
+% %Calculo de Autovalores
+% 
+% autoval=eig(P);
+% [autovec,D]=eig(P);
+% constantes = autovec\Xant;
+% %Se lleva a la forma general de la solución 
+% %x(t)=K1*A1*e^(lamda1*t)+K2*A2*e^(lamda2*t)
+% 
+% syms t;
+% 
+% v1=constantes(1)*autovec(1,1)*exp(autoval(1)*t)+constantes(2)*autovec(1,2)*exp(autoval(2)*t);
+% v2=constantes(1)*autovec(2,1)*exp(autoval(1)*t)+constantes(2)*autovec(2,2)*exp(autoval(2)*t);
+% v3=v1+v2;
+% 
+% pretty(v1)
+% pretty(v2)
+% pretty(v3)
+% 
+% figure(1);
+% fplot(v1,[ti,tf]);
+% hold on;
+% fplot(v2,[ti,tf]);
+% 
+% fplot(v3,[ti,tf]);
+% hold off;
+% title('Solución mediante calculo de autovalores');
+
+figure(2);
+%Método RK4
+it=1;
+for i= ti:h:tf
+   %Fuente variable
+   E(it,1)=10*cos(w*i);
+   
+   %Se calcula el valor de la matriz u para cada punto 
+   u=[0;E(it,1)/R1];
+   q=M\u;
+   
+   %Acá comienza el método 
+    k1= (q+P*Xant).*h;
+    Xant2=Xant+(k1.*0.5);
+    k2=(q+P*Xant2).*h;
+    Xant3=Xant+(k2.*0.5);
+    k3=(q+P*Xant3).*h;
+    Xant4=Xant+k3;
+    k4=(q+P*Xant4).*h;
+    X=Xant+(k1+2.*k2+2.*k3+k4)/6;
+    solu=[solu X];
+    Y=X(1)+X(2);
+    Xant=X;
+    it=it+1;
+    
+    %Al tener un loop de capacitores
+     
+    %Gráfica 
+    
+   plot(i,X(1),'*r',i,X(2),'*b',i,Y(1),'*g');
+    title('Solución Respuesta temporal mediante RK4');
+    xlabel('Tiempo');
+    ylabel('Tensión/Corriente');
+    hold on;
+   drawnow limitrate; 
+end
+hold off
